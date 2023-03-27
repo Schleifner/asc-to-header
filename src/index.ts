3674696774a8bfb0683eb8aa0003089116728ea4
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-03-27 16:59:12 +0800

    print ast build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello world");
+import { Program, Options, ASTBuilder } from "../node_modules/assemblyscript/dist/assemblyscript.js";
+import * as fs from 'fs';
+const program = new Program(new Options());
+const parser = program.parser;
+const sourceText = fs.readFileSync("/home/jesse/workspace/asc-to-header/test/types/env.ts", { encoding: "utf8" }).replace(/\r?\n/g, "\n");
+parser.parseFile(sourceText, "env.ts", true);
+const serializedSourceText = ASTBuilder.build(program.sources[0]);
+console.log(serializedSourceText);
