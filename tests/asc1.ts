1235ff3ea4199117894a02e624b15dc859b45eb4
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-04-03 14:56:37 +0800

    coverage lint

---
+++ tests/asc1.ts
@@ -0,0 +1,14 @@
+enum Status10 {
+    OK = 0,
+    FAIL = 1,
+}
+
+enum Status16 {
+    OK = 0x0,
+    FAIL = 0xff,
+}
+
+export type CustomCallback = (userData: i32) => void;
+
+export declare function Test(a: i32, b: i64, c: u32, d: u64, e: i16, f: u16, g: u8, h: i8, i: boolean, j: Status10, k: Status16, CustomCallback: i32, m:f32, n:f64);
+export declare function Test2(CustomCallback: u32);
