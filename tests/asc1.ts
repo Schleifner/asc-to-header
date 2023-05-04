f5c4607917aca3ea1876b13f32b7b2d23dfc1bdb
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-05-04 14:13:58 +0800

    fix coverage

--- tests/asc1.ts
+++ tests/asc1.ts
@@ -15,4 +15,4 @@ enum Another {
 export type CustomCallback = (userData: i32) => void;
 
 export declare function Test(a: i32, b: i64, c: u32, d: u64, e: i16, f: u16, g: u8, h: i8, i: boolean, j: Status10, k: Status16, CustomCallback: i32, m:f32, n:f64);
-export declare function Test2(CustomCallback: u32);
+export declare function Test2(CustomCallback: u32): u32;
--- tests/asc1.ts.fixture.h
+++ tests/asc1.ts.fixture.h
@@ -17,7 +17,7 @@ FAIL=0x1,
 };
 typedef void (*CustomCallback) (int32_t userData );
  __attribute__((import_module("asc1"))) void Test(int32_t a ,int64_t b ,uint32_t c ,uint64_t d ,int16_t e ,uint16_t f ,uint8_t g ,int8_t h ,bool i ,Status10 j ,Status16 k ,CustomCallback CustomCallback ,float m ,double n );
- __attribute__((import_module("asc1"))) void Test2(CustomCallback CustomCallback );
+ __attribute__((import_module("asc1"))) uint32_t Test2(CustomCallback CustomCallback );
 
 }
 #endif
